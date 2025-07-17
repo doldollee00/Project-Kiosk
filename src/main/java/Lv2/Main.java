3965b0f40b440e69f156e20e7c64818a1fd8fe0e
doldollee00 <doldollee00@gmail.com>
2025-07-17 09:18:04 +0900

    객체 생성 및 객체 추가 후 하나씩 출력까지 완성

--- src/main/java/Lv2/Main.java
+++ src/main/java/Lv2/Main.java
@@ -1,42 +1,63 @@
 package Lv2;
 
-import java.util.Scanner;
-import java.util.List;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        //System.out.println("Hello, Kiosk!");
+
+        MenuItem menuItem1 = new MenuItem("뉴욕버거", 10000, "비싸다");
+        MenuItem menuItem2 = new MenuItem("서울버거", 8000, "평균이다");
+        MenuItem menuItem3 = new MenuItem("부산버거", 5000, "저렴하다");
+        MenuItem menuItem4 = new MenuItem("울산버거", 1000000, "강력이다");
+
+        // List 선언 및 초기화
+        List<MenuItem> menuItems = new ArrayList<>();
+
+        // add 함수를 통해 new MenuItem(이름, 가격, 설명) List에 삽입
+        menuItems.add(menuItem1);
+        menuItems.add(menuItem2);
+        menuItems.add(menuItem3);
+        menuItems.add(menuItem4);
+
+        // Scanner 선언
         Scanner sc = new Scanner(System.in);
 
-        List<String> name = new ArrayList<>();
-        List<Integer> price = new ArrayList<>();
-        List<String> info = new ArrayList<>();
+        // 반복문을 활용해 List 안에 있는 MenuItem을 하나씩 출력
 
-        while(true){
-            //버거 메뉴 만들기
-            System.out.print("버거 이름을 입력하세요. : ");
-            String burgerName = sc.next();
-            System.out.print("버거 가격을 입력하세요. : ");
-            int burgerPrice = sc.nextInt();
-            sc.nextLine();
-            System.out.print("버거 설명을 입력하세요. : ");
-            String burgerInfo = sc.nextLine();
+        for (int i = 0; i < menuItems.size(); i++) {
+            MenuItem menulist = menuItems.get(i);
+            System.out.println((i+1) + ".  " + menulist.name + "  |  " + menulist.price + "  |  " + menulist.info);
+        }
+        System.out.println("------------------------------------");
+        while(true) {
+            // 숫자를 입력 받기
+            System.out.println("번호를 선택 (0은 종료) : ");
+            int number = sc.nextInt();
 
-            name.add(burgerName);
-            price.add(burgerPrice);
-            info.add(burgerInfo);
-
-            System.out.println("[ DingaBurger Menu ]");
-
-            for(int i=0; i<name.size(); i++){
-                System.out.println(i + ".  " + name.get(i) + "\t| W " + price.get(i) + "\t| " + info.get(i));
+            // 입력된 숫자에 따른 처리
+            // 선택한 메뉴 : 이름, 가격, 설명
+            switch(number) {
+                case 1:
+                    System.out.println(menuItems.get(0));
+                    break;
+                case 2:
+                    System.out.println(menuItems.get(0));
+                    break;
+                case 3:
+                    System.out.println(menuItems.get(1));
+                    break;
+                case 4:
+                    System.out.println(menuItems.get(2));
+                    break;
             }
-            System.out.println("0.  종료 \t\t| 종료");
-            int endBunho = sc.nextInt();
-            if (endBunho == 0) {
+            // 프로그램을 종료
+            if(number == 0){
+                System.out.println("종료");
                 break;
             }
         }
     }
 }
+
